2d4b6ceb9f6dad60dd29c1cc2827aa1c303755bb
SkyXMoon <skyxmoon@hotmail.com>
2026-06-02 10:57:27 +0800

    简化角色列表排序栏

--- app.js
+++ app.js
@@ -1221,7 +1221,7 @@ function renderCharacters() {
   const pickedIds = new Set(getTeamState().filter(Boolean).map((character) => character.id));
   const fragment = document.createDocumentFragment();
   const characters = getFilteredCharacters();
-  els.listCount.textContent = `${characters.length} / ${CHARACTERS.length} 名角色`;
+  els.listCount.textContent = `${characters.length}/${CHARACTERS.length} 名角色`;
 
   characters.forEach((character, index) => {
     const tile = document.createElement("button");
