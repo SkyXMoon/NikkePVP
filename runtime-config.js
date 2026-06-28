4c46d36f146177e51fee5cb4c1e41a019f5ffbfd
SkyXMoon <skyxmoon@hotmail.com>
2026-06-28 11:13:34 +0800

    更新前端发布产物

--- runtime-config.js
+++ runtime-config.js
@@ -4,5 +4,5 @@ window.__NIKKE_CONFIG__ = {
   "supabaseUrl": "https://xjdyqxkryqtkiroylygp.supabase.co",
   "functionsBaseUrl": "https://xjdyqxkryqtkiroylygp.supabase.co/functions/v1",
   "publishableKey": "sb_publishable_b5uqRu8YvXZSYoOJu-qScw_VKXtGlYD",
-  "generatedAt": "2026-06-28T02:47:48.687Z"
+  "generatedAt": "2026-06-28T03:13:33.366Z"
 };
